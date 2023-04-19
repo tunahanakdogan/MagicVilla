2e7a8e9608d9b904dda1f7b2e470bf39883275c9
Tunahan Akdogan <tunahan.akdogan@siskon.com.tr>
2023-04-19 13:56:39 +0300

    Villa model updated

--- MagicVilla/Models/Villa.cs
+++ MagicVilla/Models/Villa.cs
@@ -4,7 +4,11 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
